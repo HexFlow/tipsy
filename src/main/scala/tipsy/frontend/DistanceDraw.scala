d0668abcfd2b36789feeb83e4524b0012d6a542e
Pallav Agarwal <pallavag@iitk.ac.in>
2017-03-08 02:47:36 +0530

    Switch to Interactive Rendering (with mouse support)

--- src/main/scala/tipsy/frontend/DistanceDraw.scala
+++ src/main/scala/tipsy/frontend/DistanceDraw.scala
@@ -8,16 +8,21 @@ import java.awt.Dimension
 import javax.swing.{ JFrame, JLabel, ImageIcon  }
 import java.io.File
 import javax.imageio.ImageIO
+import javax.swing.JFrame;
+
 
 import forcelayout.renderer.ImageRenderer
+import forcelayout.renderer._
 import forcelayout.{ Node, Edge, SpringGraph }
 
 object DistanceDraw {
   def apply(network: List[(Int, Int, Double)], length: Int) {
     println(network)
+    val avg = network.map(_._3).sum/network.size
+    println(avg)
     val nodes = (1 to length).map(x => Node(""+x, "Node "+x))
     val edges = network.map{
-      case (a, b, c) => Edge(nodes(a), nodes(b), c)
+      case (a, b, c) => Edge(nodes(a), nodes(b), c-avg/4)
     }
 
     val graph = new SpringGraph(nodes, edges)
@@ -34,10 +39,20 @@ object DistanceDraw {
       onComplete = (it => {
         println("Completed in " + it + " iterations")
         // Renders a 500x500 pixel image of the final graph layout
-        val image = ImageRenderer.drawGraph(graph, 500, 500)
+		//val image = ImageRenderer.drawGraph(graph, 500, 500)
 
-        // Writes the image to a PNG file
-        ImageIO.write(image, "png", new File("forcegraph.png"))
+		// Writes the image to a PNG file
+		//ImageIO.write(image, "png", new File("forcegraph.png"))
+		val vis = new BufferedInteractiveGraphRenderer(graph)
+		val frame = new JFrame("Les Miserables")
+		frame.setSize(920, 720)
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+		frame.getContentPane().add(vis) 
+		frame.pack()
+
+		// Pops up the JFrame on the screen, and starts the layout process
+		frame.setVisible(true)
+		vis.start
       }),
       onIteration = (it => imgLabel.setIcon(new ImageIcon(ImageRenderer.drawGraph(graph, 500, 500))))
     )
