6da9a2ee75ab5b093358a88f236ad7d2b8c901b4
Saksham Sharma <saksham0808@gmail.com>
2017-04-05 10:53:06 +0530

    Use traditional form of CLI parsing

--- src/main/scala/tipsy/Tipsy.scala
+++ src/main/scala/tipsy/Tipsy.scala
@@ -1,35 +1,39 @@
 package tipsy
 
 import tipsy.frontend._
-import scala.collection.mutable.{Map => mMap}
+import scala.collection.mutable.{Map => mMap, Stack}
 
 object Tipsy {
   def main(args: Array[String]): Unit = {
-    val options = args
-      .filter(_.startsWith("-"))
-      .map(_.stripPrefix("-"))
-      .map(_.stripPrefix("-"))
     val files = args.filter(!_.startsWith("-"))
 
     var optset: mMap[CLIMode, String] = mMap()
     var web: Boolean = false
 
-    options.foreach {
-      case "pp" => optset put (PRINTPARSE, "")
-      case "pf" => optset put (PRINTFLOW, "")
-      case "dp" => optset put (DRAWPARSE, "")
-      case "df" => optset put (DRAWFLOW, "")
-      case "le" => optset put (LEASTEDIT, "")
-      case msg if msg.startsWith("len") => optset put (LEASTEDITLIMIT, msg.split('=')(1))
-      case "web" => web = true
-      case unknown => println("[Warning] Ignoring unknown argument: " + unknown)
+    var i = 0
+    var mFiles: Stack[String] = Stack()
+    while (i < args.length) {
+      args(i) match {
+        case "-pp" => optset put (PRINTPARSE, "")
+        case "-pf" => optset put (PRINTFLOW, "")
+        case "-dp" => optset put (DRAWPARSE, "")
+        case "-df" => optset put (DRAWFLOW, "")
+        case "-le" => optset put (LEASTEDIT, "")
+        case msg if msg.startsWith("-len=") =>
+          optset put (LEASTEDITLIMIT, msg.split('=')(1))
+        case "-web" => web = true
+        case msg if msg.startsWith("-") =>
+          println("[Warning] Ignoring unknown argument: " + msg)
+        case file => mFiles = mFiles.push(file)
+      }
+      i += 1
     }
 
     // The toList thing is Scala-fu to convert mutable to immutable
     if (web) {
       Web(Set())
     } else {
-      CLI(files, Map(optset.toList: _*))
+      CLI(mFiles.toArray, Map(optset.toList: _*))
     }
   }
 }
