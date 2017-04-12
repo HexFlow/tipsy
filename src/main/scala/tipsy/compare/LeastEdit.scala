8290ddcc7579a62d784f58837c195954bf93b146
Saksham Sharma <saksham0808@gmail.com>
2017-04-12 15:37:53 +0530

    WIP: Diff creation

--- src/main/scala/tipsy/compare/LeastEdit.scala
+++ src/main/scala/tipsy/compare/LeastEdit.scala
@@ -3,6 +3,24 @@ package tipsy.compare
 import tipsy.parser._
 import scalaz._
 
+sealed trait DiffChange
+case object ADD_d extends DiffChange
+case object DEL_d extends DiffChange
+
+case class Diff (
+  change: DiffChange,
+  entry: CFEnum
+)
+
+case class EditRet (
+  diffs: List[Diff],
+  dist: Int
+) {
+  def correct(d: Diff, dis: Int): EditRet = {
+    this.copy(diffs = d :: diffs, dist = dis + dist)
+  }
+}
+
 object LeastEdit {
   def apply(trees: List[ParseTree]): List[(Int, Int, Double)] = {
     if (trees.length < 2) {
@@ -15,7 +33,7 @@ object LeastEdit {
         val v2 = FlowGraphTweaks(t2._1.compress).toVector
         val k = (editDist(v1, v2, v1.length, v2.length))
         println(k)
-        (t1._2, t2._2, 1/(k.toDouble+0.1))
+        (t1._2, t2._2, 1/(k.dist.toDouble+0.1))
       }
       case _ => ???
     }.toList
@@ -30,36 +48,49 @@ object LeastEdit {
       val v1 = FlowGraphTweaks(prog.compress).toVector
       val v2 = FlowGraphTweaks(tree.compress).toVector
       val k = (editDist(v1, v2, v1.length, v2.length))
-      (tree, 1/(k.toDouble+0.1))
+      (tree, 1/(k.dist.toDouble+0.1))
     }.toList
   }
 
-  def editDistRecur[T](
-    s1: Vector[T], s2: Vector[T], m: Int, n: Int
-  ): Int = {
-    if (m == 0) n*20
-    else if (n == 0) m*20
+  def editDistRecur(
+    s1: Vector[CFEnum], s2: Vector[CFEnum], m: Int, n: Int
+  ): EditRet = {
+    if (m == 0) (???, n*20)
+    else if (n == 0) (???, m*20)
     else if (s1(m-1) == s2(n-1))
       editDist(s1, s2, m-1, n-1)
     else {
       var d = 100000000
-      s1(m-1) match {
-        case POSTEXPR(y) => {
-          s2(n-1) match {
-            case POSTEXPR(z) => {
-              d = editDist(y.toVector, z.toVector,
-                y.length, z.length)/Math.max(y.length, z.length)
-            }
-            case _ =>
-          }
+
+      (s1(m-1), s2(n-1)) match {
+        case (POSTEXPR(y), POSTEXPR(z)) => {
+          d = editDist(y.toVector, z.toVector,
+            y.length, z.length)/Math.max(y.length, z.length)
         }
         case _ =>
       }
-      val ret = Math.min(20 + List(
-        editDist(s1, s2, m, n-1),
-        editDist(s1, s2, m-1, n),
-        editDist(s1, s2, m-1, n-1)
-      ).reduceLeft(_ min _), d + editDist(s1, s2, m-1, n-1))
+
+      val opt1 = editDist(s1, s2, m, n-1)
+        .correct(Diff(ADD_d, s2(n-1)), 20)
+
+      val opt2 = editDist(s1, s2, m-1, n)
+        .correct(Diff(DEL_d, s1(m-1)), 20)
+
+      val opt3 = editDist(s1, s2, m-1, n-1)
+        .correct(Diff(ADD_d, s2(n-1)), 20)
+        .correct(Diff(DEL_d, s1(m-1)), 20)
+
+      val opt4 = editDist(s1, s2, m-1, n-1)
+        .correct(Diff(ADD_d, s2(n-1)), d/2)
+        .correct(Diff(DEL_d, s1(m-1)), d/2)
+
+      val ret = List(
+        opt1,
+        opt2,
+        opt3,
+        opt4
+      ).reduceLeft(_.dist min _.dist)
+
       //if(m!=0 && n!=0)
       //println("Now comparing " + s1(m-1) + " " + s2(n-1))
       //println("Reuturning: " + ret)
