e05c62de27b91a0100acf1b023d401d0fa5d5805
Saksham Sharma <saksham0808@gmail.com>
2017-12-28 01:22:20 +0530

    Initial implementation of positions in CF list.

--- src/main/scala/tipsy/compare/ProgStats.scala
+++ src/main/scala/tipsy/compare/ProgStats.scala
@@ -11,26 +11,26 @@ object ProgStats {
   def apply(code: ParseTree): Stats = {
 
     val counts = code.compress.collect {
-      case x @ (IFCOND | FUNC | LOOPCOND) => x
+      case x @ (IFCOND() | FUNC() | LOOPCOND()) => x
     }.groupBy(identity).mapValues(_.size)
 
     var depth = 0
     var maxdepth = 0
     val blocks = code.compress.collect {
-      case x @ (BLOCKOPEN | BLOCKCLOSE) => x
+      case x @ (BLOCKOPEN() | BLOCKCLOSE()) => x
     }.foreach {
-      case BLOCKOPEN =>
+      case BLOCKOPEN() =>
         depth = depth + 1
         maxdepth = Integer.max(maxdepth, depth)
-      case BLOCKCLOSE =>
+      case BLOCKCLOSE() =>
         depth = depth - 1
       case _ => ???
     }
 
     Stats(
-      ifs = Some(counts get IFCOND getOrElse 0),
-      loops = Some(counts get LOOPCOND getOrElse 0),
-      fxns = Some(counts get FUNC getOrElse 0),
+      ifs = Some(counts get IFCOND() getOrElse 0),
+      loops = Some(counts get LOOPCOND() getOrElse 0),
+      fxns = Some(counts get FUNC() getOrElse 0),
       depth = Some(maxdepth)
     )
   }
